e5315cdcf5474015221d6b3595f5cc66ae45d160
Karang <arthur.hennequin@free.fr>
2014-01-02 19:27:32 +0100

    Fix communication with simulator

--- src/fr/helium/simulator/server/GCSclient.java
+++ src/fr/helium/simulator/server/GCSclient.java
@@ -1,6 +1,10 @@
 package fr.helium.simulator.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 import fr.helium.simulator.Quadcopter;
 
@@ -8,38 +12,48 @@ public class GCSclient extends Thread {
 
 	private final Socket socket;
 	private final Quadcopter uav;
+	private BufferedReader in;
 	
 	public GCSclient(Socket s, Quadcopter uav) {
 		this.uav = uav;
 		this.socket = s;
+		
+		try {
+			in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ByteBuffer decodeMsg(String msg) {
+		return ByteBuffer.wrap(msg.getBytes());
 	}
 	
 	@Override
 	public void run() {
 		while (!isInterrupted()) {
 			try {
-				if (socket.getInputStream().available() >= 2) {
-					char c = (char)socket.getInputStream().read();
-					if (c=='T' || c=='P' || c=='Y' || c=='R') {
-						int i = socket.getInputStream().read();
-						float v = (i-127) / 127f;
-						
-						if (c == 'T') { // Thrust
-							uav.thrust_ref = v;
-							System.out.println("Thrust: "+v);
-						} else if (c == 'P') { // Pitch
-							uav.pitch_ref = v*360;
-							//System.out.println("Pitch: "+v*360);
-						} else if (c == 'Y') { // Yaw
-							uav.yaw_ref = v*360;
-							//System.out.println("Yaw: "+v*360);
-						} else if (c == 'R') { // Roll
-							uav.roll_ref = v*360;
-							//System.out.println("Roll: "+v*360);
-						}
+				ByteBuffer msg = decodeMsg(in.readLine());
+				char c = msg.getChar();
+				if (c=='T' || c=='P' || c=='Y' || c=='R') {
+					int i = msg.getInt();
+					float v = Float.intBitsToFloat(i);
+					
+					if (c == 'T') { // Thrust
+						uav.thrust_ref = v;
+						//System.out.println("Thrust: "+v);
+					} else if (c == 'P') { // Pitch
+						uav.pitch_ref = v;
+						//System.out.println("Pitch: "+v);
+					} else if (c == 'Y') { // Yaw
+						uav.yaw_ref = v;
+						//System.out.println("Yaw: "+v);
+					} else if (c == 'R') { // Roll
+						uav.roll_ref = v;
+						//System.out.println("Roll: "+v);
 					}
 				}
-			} catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
